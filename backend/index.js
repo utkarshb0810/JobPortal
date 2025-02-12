bdb622514075219c0a952dac1db557e7ff87898b
Utkarsh Barnawal <utkarshbarnawal8@gmail.com>
2025-02-12 18:33:31 +0530

    Changed the api end points after deployment

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://localhost:5173",
+  origin: "https://jobportal-r1ee.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,5 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:5000/api/v1/job";
+export const USER_API_END_POINT =
+  "https://jobportal-r1ee.onrender.com//api/v1/user";
+export const JOB_API_END_POINT =
+  "https://jobportal-r1ee.onrender.com//api/v1/job";
 export const Application_API_END_POINT =
-  "http://localhost:5000/api/v1/application";
-export const Company_API_END_POINT = "http://localhost:5000/api/v1/company";
+  "https://jobportal-r1ee.onrender.com//api/v1/application";
+export const Company_API_END_POINT =
+  "https://jobportal-r1ee.onrender.com//api/v1/company";
